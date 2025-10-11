88ab0f2c94dde32eb9f9bd279fac85b20057d6ca
SDinee <sidne.correiacabo@gmail.com>
2025-10-11 17:19:06 -0300

    leitura com espaços

---
+++ LinguagemC/basico/cadalunfgets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    printf("Segunda aula de Programação e C");
+    printf("\n*****************************");
+
+    printf("\nProf Alessandro Calin");
+    printf("\n*****************************");
+    printf("\nCadastrando Aluno:");
+
+    // char de vetor
+    char nome[40];
+    int idade;
+
+    // leitura com espaços
+    printf("\nDigite o nome do Aluno: ");
+    fgets(nome, sizeof(nome), stdin);
+
+    printf("\nDigite a idade do Aluno: ");
+    scanf("%d", &idade);
+
+    printf("\n**********************************");
+    printf("\nRelatório");
+    printf("\n**********************************");
+
+    printf("\nNome: %s", nome);
+    printf("\nIdade: %d", idade);
+
+    return 0;
+}
