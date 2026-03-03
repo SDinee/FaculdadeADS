39264331d4bb264095c5a7bf2bd9931ad19212a4
Sidne <sidne.correiacabo@gmail.com>
2026-03-03 19:25:06 -0300

    testando import de arquivo

--- Python/praticando/main .py
+++ Python/praticando/main .py
@@ -1,4 +1,8 @@
-
 print ('1 - Calculadora')
 
-opcao = (input('Escolha uma opção:'))
+opcao = (input('Escolha uma opção: '))
+
+
+#importando o arquivo calculadora.py  (não precisa de .py)
+if opcao == '1':
+    __import__('calculadora')
