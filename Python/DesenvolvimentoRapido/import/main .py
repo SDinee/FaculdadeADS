564c65d2255b7d1ff1b9ca1b369a59c62ecd5f78
Sidne <sidne.correiacabo@gmail.com>
2026-04-02 13:01:49 -0300

    ajuste

--- Python/DesenvolvimentoRapido/import/main .py
+++ Python/DesenvolvimentoRapido/import/main .py
@@ -2,7 +2,7 @@ print ('1 - Calculadora')
 print ('2 - criar uma lista')
 
 
-opcao = (input('Escolha uma opção: ')).strip()
+opcao = input('Escolha uma opção: ').strip()
 
 
 #importando o arquivo calculadora.py  (não precisa de .py)
