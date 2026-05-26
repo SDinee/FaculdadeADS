ecb4c387847537d4f51d390127c8b2c4fbc0bee0
Sidne <sidne.correiacabo@gmail.com>
2026-05-26 15:17:54 -0300

    Interface de um relógio utilizando after

---
+++ Python/DesenvolvimentoRapido/Tkinter/RelogioAfter.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from datetime import datetime
+
+def atualizar_relogio():
+    agora = datetime.now().strftime("%H:%M:%S")
+    label.config(text=agora)
+    root.after(1000, atualizar_relogio) #agenda repetição
+    
+root = tk.Tk()
+root.title("Relógio com after()")
+label = tk.Label(root, font=("Consolas", 32))
+label.pack(padx=20, pady=20)
+atualizar_relogio()
+root.mainloop()
