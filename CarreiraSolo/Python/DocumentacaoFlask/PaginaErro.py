c521e7e6403dbf7819ef56e81ed1a4d4d0b6b1ad
Sidne <sidne.correiacabo@gmail.com>
2026-05-26 23:23:01 -0300

    remoção de estudos aparte, criado um repositório somente para estes

--- CarreiraSolo/Python/DocumentacaoFlask/PaginaErro.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-
-app = Flask(__name__)
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('PageNotFound.html'), 404
-
-#Aqui o Flask renderiza page_not_found.html e retorna o status 404.
--- CarreiraSolo/Python/DocumentacaoFlask/Redirecionar.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,redirect, url_for
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return redirect(url_for('login'))
-
-#redirect() → envia o usuário para outra rota.
