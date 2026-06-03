46f579cfc614aac4832335acde77171d9a9c3082
Sidne <sidne.correiacabo@gmail.com>
2026-06-03 13:53:03 -0300

    DB evento

--- SQL/PostgreSQL/Evento/schema.sql
+++ SQL/PostgreSQL/Evento/schema.sql
@@ -9,21 +9,3 @@ CREATE TABLE inscricao_evento_b (
   aluno_nome VARCHAR(80),
   curso      VARCHAR(50)
 );
-
-INSERT INTO inscricao_evento_a (aluno_nome, curso) VALUES
-  ('Alice Mendes',     'ADS'),
-  ('Bruno Carvalho',   'CC'),
-  ('Carla Dias',       'GTI'),
-  ('Daniel Faria',     'SI'),
-  ('Eliana Gomes',     'ADS'),
-  ('Fábio Henrique',   'CC'),
-  ('Gabriela Lopes',   'GTI');
-
-INSERT INTO inscricao_evento_b (aluno_nome, curso) VALUES
-  ('Alice Mendes',     'ADS'),
-  ('Bruno Carvalho',   'CC'),
-  ('Henrique Martins', 'SI'),
-  ('Iris Castro',      'ADS'),
-  ('João Lima',        'CC'),
-  ('Fábio Henrique',   'CC'),
-  ('Karen Souza',      'GTI');
