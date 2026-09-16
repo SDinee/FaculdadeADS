d400507beee389edc6ca73e9bea03339d6e1148b
Sidne <sidne.correiacabo@gmail.com>
2026-09-16 16:51:31 -0300

    adiciona exemplo de array com numpy.

---
+++ Python/BigData/numpy/listanum.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+lista_num = np.array([1,2,3,4,5,6])
+print("Array:", (lista_num))
+print("Tipo:", type(lista_num))
+print("Dimensão: ", lista_num.ndim)
+print("Tamanho: ", lista_num.size)
