0e56db2f3d512e858045f5a6b37afd154cade6f7
Sidne <sidne.correiacabo@gmail.com>
2026-09-18 20:37:38 -0300

    feat(python): exemplos de arrays com numpy

---
+++ Python/BigData/numpy/arrays.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([1,2,3])
+b = np.array([1,2,3]),([4,5,6])
+
+zeros = np.zeros((2,3))
+ones = np.ones((3,3))
+
+seq = np.arange(0,10,2)
+linspace = np.linspace(0,1,5)
+
+print("A: ", a)
+print("B: ", b)
+print("Zeros: ", zeros)
+print("Ones: ", ones)
+print("Seq: ", seq)
+print("Linspace: ", linspace)
