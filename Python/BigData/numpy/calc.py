130b21baefb2cd279196d494333d04a3a5d60178
Sidne <sidne.correiacabo@gmail.com>
2026-09-20 17:20:37 -0300

    feat(python): adiciona arquivo calc.py com operações básicas usando numpy

---
+++ Python/BigData/numpy/calc.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+x = np.array([1,2,3,4,5])
+y = np.array([10,20,30,40,50])
+
+print("Soma: ", x + y)
+print("Mult: ", x * y)
+print("Quadrado: ", x **2)
+print("Seno: ", np.sin(x))
+print("Média: ", np.mean(x))
+print("Desvio padrão: ", np.std(x))
